bd0086500ad9fbb923890f44f15b5234f8aead13
Ronan Murphy <ronan_murfy@hotmail.com>
2017-02-01 00:15:46 +0000

    Added todo app with basic Task model.

---
+++ todo/models.py
@@ -0,0 +1,42 @@
+from __future__ import unicode_literals
+
+from django.contrib.auth.models import User
+from django.db import models
+from django.utils import timezone
+
+
+class Task(models.Model):
+	owner = models.ForeignKey(
+		User,
+		related_name="owner",
+		on_delete=models.CASCADE
+	)
+	marked_complete_by = models.ForeignKey(
+		User,
+		related_name="marked_complete_by",
+		on_delete=models.CASCADE
+	)
+	title = models.CharField(max_length=100)
+	description = models.TextField()
+	task_open = models.BooleanField(default=True)
+	created_date = models.DateTimeField("date created")
+	completed_date = models.DateTimeField("date completed")
+
+	@classmethod
+	def create(cls, owner, title, description):
+		"""
+		Utility method to fill in default values upon creation.
+		"""
+		return Task(
+			owner=owner,
+			marked_complete_by=None,
+			title=title,
+			description=description,
+			task_open=True,
+			created_date=timezone.now(),
+			completed_date=None
+		)
+
+
+	def __str__(self):
+		return self.title
