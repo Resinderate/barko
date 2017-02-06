fe2a6f8120a5215e024dcac92173006780b5c838
Ronan Murphy <ronan_murfy@hotmail.com>
2017-02-06 01:03:54 +0000

    Added js functions to handle posting data on button clicks.

--- static/js/barko.js
+++ static/js/barko.js
@@ -1,4 +1,42 @@
-// Prepare the tooltips on all buttons.
 $(document).ready(function(){
-    $('[data-toggle="tooltip"]').tooltip(); 
+
+    // Prepare the tooltips on all buttons.
+    $('[data-toggle="tooltip"]').tooltip();
+
+    // Press complete button and complete the task.
+    $('.complete-task').click(function() {
+
+        var task_id = $(this).data("task-id")
+
+        $.post(
+            "/todo/task/",
+            { "_method": "PUT",
+              "task_id": task_id,
+              "completed": "true",
+              "csrfmiddlewaretoken": CSRF_TOKEN
+            },
+            function() { window.location.reload(true); }
+        );
+    });
+
+    // Press task delete button and delete the task.
+    $('.delete-task').click(function() {
+
+        var task_id = $(this).data("task-id")
+
+        $.post(
+            "/todo/task/",
+            { "_method": "DELETE",
+              "task_id": task_id,
+              "csrfmiddlewaretoken": CSRF_TOKEN
+            },
+            function() { window.location.reload(true); }
+        );
+    });
+
+
+    // Toggle hide all task panels that are completed.
+    $('#hide-completed').click(function() {
+        $('.task-completed').toggle()
+    });
 });
